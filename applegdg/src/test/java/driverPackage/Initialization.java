1958ce1137c5837c39388cc5b75541a68ac0927e
Mizanur Rahaman <mizanur35-1657@diu.edu.bd>
2024-01-18 15:25:04 +0600

    webdrivermanager edit

--- applegdg/src/test/java/driverPackage/Initialization.java
+++ applegdg/src/test/java/driverPackage/Initialization.java
@@ -1,21 +1,18 @@
 package driverPackage;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Initialization {
     public WebDriver driver;
 
     @BeforeTest
     public void launchBrowser() {
-
-//        String chromedriverlocation = "G:/New Selenium/chromedriver/chromedriver.exe";
-        String firefoxdriverlocation = "G:/New Selenium/geckodriver/geckodriver.exe";
-        System.setProperty("webdriver.chrome.driver", firefoxdriverlocation);
+        WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         System.out.println("Browser is ready");
         driver.manage().window().maximize();
